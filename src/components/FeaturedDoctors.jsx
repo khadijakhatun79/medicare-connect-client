a707077c8bb0c9e3cc4e1e8f73b090047e9735c9
khadijakhatun79 <khadijakhatunnurse@gmail.com>
2026-06-25 21:58:14 +0600

    url issue fix

--- src/components/FeaturedDoctors.jsx
+++ src/components/FeaturedDoctors.jsx
@@ -16,7 +16,7 @@ export default function FeaturedDoctors() {
     const fetchDoctors = async () => {
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}doctors`
+          `${process.env.NEXT_PUBLIC_API_URL}/doctors`
         );
 
         const data = await res.json();
